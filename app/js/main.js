b42cb8a59f2cec1e5af8b11cd6c553a257481d82
varekai-dev <serhijsav@gmail.com>
2020-03-05 16:55:28 +0200

    finished new item page

--- app/js/main.js
+++ app/js/main.js
@@ -16,6 +16,17 @@ $(function(){
 
       });
 
+      
+    $(".js-range-slider").ionRangeSlider({
+      type: "double",
+       
+        min: 0,
+        max: 1000,
+        from: 0,
+        to: 600,
+        prefix: "$"
+  });
+
     
     var mixer = mixitup('.products__inner-box');
 
